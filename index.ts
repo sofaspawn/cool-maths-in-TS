c91b86fc4f29f5c03593161c166e8831b89a0191
sofaspawn <h.m1nus.work@gmail.com>
2024-08-29 11:04:43 +0530

    readme

--- index.ts
+++ index.ts
@@ -1,9 +1,46 @@
 const canvas = document.getElementById('canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
 
-function draw(){
-    for(let x=0; x<canvas.width; x++){
-        for ()
+let doIStop = false;
+
+if(!ctx){
+    throw new Error("ctx error");
+}
+
+const imageData = ctx.createImageData(1,1);
+const data = imageData.data;
+
+data[0] = 255;
+data[1] = 255;
+data[2] = 255;
+data[3] = 255;
+
+function handleKeyPress(event: KeyboardEvent){
+    if (event.key=='q'){
+        doIStop = true;
     }
 }
+
+function draw(){
+    if(!ctx){
+        throw new Error("ctx error");
+    }
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    for(let x=0; x<canvas.width; x++){
+        for (let y=0; y<canvas.height; y++){
+            if (Math.random()<0.01){
+                ctx.putImageData(imageData, x, y);
+            }
+        }
+    }
+
+    if (!doIStop){
+        requestAnimationFrame(draw);
+    }
+}
+
+window.addEventListener("keypress", handleKeyPress);
+draw();
 
--- readme.md
+++ readme.md
@@ -2,6 +2,6 @@
 Basically, building some kinda math animations on the browser, from scratch, using only typescript.
 
 ## TODO:
-- [ ] lightup random pixels
+- [x] lightup random pixels
 - [ ] figure out the mandelbrot equation
 - [ ] draw the mandelbrot equation
